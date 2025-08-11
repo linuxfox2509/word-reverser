bf475d88fef0f3ef8241ecf8a9655b4d3720168f
LinuxFox <julian.muenchberg@web.de>
2025-08-11 23:39:52 +0200

    You can now reverse words but keep their order with -w

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,27 @@ use clap::Parser;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
-
 struct Args {
-    text: String,
+    text: Vec<String>,
+
+    #[arg(short = 'w', long = "words", help = "Reverse words but keep their order")]
+    words: bool,
 }
 
 fn main() {
     let args = Args::parse();
-    let reversed: String = args.text.chars().rev().collect();
-    println!("{}", reversed);
+
+    let input = args.text.join(" ");
+
+    let output = if args.words {
+        input
+            .split_whitespace()
+            .map(|word| word.chars().rev().collect::<String>())
+            .collect::<Vec<String>>()
+            .join(" ")
+    } else {
+        input.chars().rev().collect()
+    };
+
+    println!("{}", output);
 }
